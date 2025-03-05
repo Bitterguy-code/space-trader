d2d3d516b520e31ce427c34f8a67501b1c22fc14
bitterguy-code <jasonb4cake@gmail.com>
2025-03-05 10:34:41 -0500

    Sanitized username

--- Server/console.py
+++ Server/console.py
@@ -5,13 +5,25 @@ from keyrings.alt.file import EncryptedKeyring
 
 keyring = EncryptedKeyring()
 app = Flask(__name__)
-
+username = ""
 
 CORS(app)
-password=keyring.get_password("space-trader", "bitterguy")
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://bitterguy:{password}@localhost/spacetrader"
+password=keyring.get_password("space-trader", "{username}")
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://{username}:{password}@localhost/spacetrader"
 
 db = SQLAlchemy(app)
 class agent(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    acount_id = db.Column(db.String(255))
+    symbol = db.Column(db.String(255))
+    headquarters = db.Column(db.String(255))
+    credits = db.Column(db.integer)
+    starting_faction = db.Column(db.String(255))
+    ship_count = db.Column(db.Integer)
+
+class useragentinfo(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    agent_id = db.Column(db.Integer)
+    agent_key = db.Column(db.String(255))
 
