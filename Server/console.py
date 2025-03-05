6f51163332ca0757a0ae9dc5f60baec82bb6f1e0
bitterguy-code <jasonb4cake@gmail.com>
2025-03-05 10:54:52 -0500

    filter test 2

--- Server/console.py
+++ Server/console.py
@@ -5,7 +5,7 @@ from keyrings.alt.file import EncryptedKeyring
 
 keyring = EncryptedKeyring()
 app = Flask(__name__)
-username = "test"
+isPhoneGap = CHANGEME
 
 CORS(app)
 password=keyring.get_password("space-trader", "{username}")
@@ -23,7 +23,7 @@ class agent(db.Model):
 
 class useragentinfo(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255))
+    isPhoneGap = CHANGEME
     agent_id = db.Column(db.Integer)
     agent_key = db.Column(db.String(255))
 
