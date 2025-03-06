2fab700244ac68a81bc88343f55508135c244263
bitterguy-code <jasonb4cake@gmail.com>
2025-03-05 23:22:20 -0500

    Clean up files from unused functions, made webpage dynamically update. Now successfully makes call to spacetraders to get agent data.

--- Server/console.py
+++ Server/console.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from keyrings.alt.file import EncryptedKeyring
+import requests
 
 #Variables
 keyring = EncryptedKeyring()
@@ -39,10 +40,17 @@ class UserAgentInfo(db.Model):
     agent = db.relationship('Agent', backref='user_agent_info',lazy=True)
 
 #API calls
+apiKey = ''
 @app.route('/api/store-key', methods=['POST'])
 def store_key():
+    """_summary_
+    receives api key submission from the client. Checks if it is valid, then if it is stored. If it is valid and not stored then it stores it. Otherwise it returns an error
+
+    Returns:
+        array: [json-object('succes/fail':'message'),http code]
+    """
     data = request.get_json()
-    api_key = data.get('apiKey')
+    api_key = data.get('new_api_key')
 
     #check if api key is empty and send error back if it is
     if not api_key:
@@ -50,13 +58,32 @@ def store_key():
     
     #TODO make this store the API key
     print(f"Recieved API key: {api_key}")
+    global apiKey
+    apiKey = api_key
+
 
     #TODO make logic that checks if the API key was actually stored
     return jsonify({'succes':'API key stored!'}), 200
 
+@app.route('/api/fetch-agent', methods=['POST'])
+def fetch_agent():
+    #setup spacetrader api call and header options
+    url = 'https://api.spacetraders.io/v2/my/agent'
+    headers = {
+        'content-type':'application/json',
+        'authorization':f'Bearer {apiKey}'
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        #TODO call a storage function to storeagent data in the sql database
+        return jsonify(response.json()), response.status_code
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error':str(e)}), 500
 
 with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000)
+    app.run(host='127.0.0.1', port=8000,debug = True)
